6b22a559098dc2b98fcf9548ef6ac8f72dbef64e
Shiva Kuppa <shivakuppa@Shivas-MacBook-Pro.local>
2022-10-12 23:14:04 -0400

    all code

--- index.js
+++ index.js
@@ -1,168 +1,175 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var exphbs = require('express-handlebars');
+var PORT = 3000;
+var app = express();
 
-var states = ["Idaho", "South Dakota", "Hawaii", "Alaska", "Alabama", "New York"];
-var total = states.length
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use('/public', express.static('public'));
+var _ = require("underscore");
+var dataUtil = require("./data-util");
 
-var abvMap = {
-    "Alabama": "01",
-    "Alaska": "02",
-    "Arizona": "04",
-    "Arkansas": "05",
-    "California": "06",
-    "Colorado": "08",
-    "Connecticut": "09",
-    "Delaware": "10",
-    "District Of Columbia": "11",
-    "Florida": "12",
-    "Georgia": "13",
-    "Hawaii": "15",
-    "Idaho": "16",
-    "Illinois": "17",
-    "Indiana": "18",
-    "Iowa": "19",
-    "Kansas": "20",
-    "Kentucky": "21",
-    "Louisiana": "22",
-    "Maine": "23",
-    "Maryland": "24",
-    "Massachusetts": "25",
-    "Michigan": "26",
-    "Minnesota": "27",
-    "Mississippi": "28",
-    "Missouri": "29",
-    "Montana": "30",
-    "Nebraska": "31",
-    "Nevada": "32",
-    "New Hampshire": "33",
-    "New Jersey": "34",
-    "New Mexico": "35",
-    "New York": "36",
-    "North Carolina": "37",
-    "North Dakota": "38",
-    "Ohio": "39",
-    "Oklahoma": "40",
-    "Oregon": "41",
-    "Pennsylvania": "42",
-    "Rhode Island": "44",
-    "South Carolina": "45",
-    "South Dakota": "46",
-    "Tennessee": "47",
-    "Texas": "48",
-    "Utah": "49",
-    "Vermont": "50",
-    "Virginia": "51",
-    "Washington": "53",
-    "West Virginia": "54",
-    "Wisconsin": "55",
-    "Wyoming": "56",
-}
+var _DATA = dataUtil.loadData().anime_post;
+
+//For mongodb connection
+var mongoose = require('mongoose');
+var dotenv = require('dotenv');
+mongoose.Promise = global.Promise
+var Review = require('./show');
+
+dotenv.config();
+console.log(process.env.MONGODB); 
+mongoose.connect(process.env.MONGODB);
+mongoose.connection.on('error', function() {
+    console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+    process.exit(1);
+});
+
+//For socket connection
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//For string validator
+var validator = require('validator');
+
+//For youtube video title
+var getYoutubeTitle = require('get-youtube-title')
 
 
-document.getElementById("button").onclick = function() {
-    clock();
-    inputValue();
-    hov();
-};
+io.on('connection', function(socket) {
+  console.log('NEW connection.');
+  socket.on('disconnect', function(){
+      console.log('Oops. A user disconnected.');
+  });
+});
 
- function clock() {
-    ti = 20;
+/* Add whatever endpoints you need! Remember that your API endpoints must
+ * have '/api' prepended to them. Please remember that you need at least 5
+ * endpoints for the API, and 5 others.
+ */
 
-   var y = setInterval(function() {
-        if (ti == 0) {
-            document.getElementById("time").innerHTML = "EXPIRED";
-            clearInterval(y);
-          }
-        else {
-            document.getElementById("time").innerHTML = ti;
-            ti--;
-        };
+app.get('/',function(req,res){
+  Review.find({}, function(err, animes) {
+    var usern = dataUtil.getNames(animes);
 
-    }, 1000);
+    res.render('home', {animes: animes, user: usern});
+  }); 
+})
 
-}
+app.get('/create', function(req, res) {
+  var usern = dataUtil.getNames(_DATA);
+  res.render('create', {user: usern});
+})
 
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+app.post('/api/create', function(req, res) {
 
-function inputValue() {
-   var x = setInterval(function() {
-    var val = document.getElementById("input").value;
+  getYoutubeTitle((req.body.clip).split('v=').pop(), function (err, title) {
+    if (validator.isURL(req.body.clip)) {
 
-    if (document.getElementById("time").innerHTML != "EXPIRED" && states.includes(val)){
-        var node = document.createElement("LI");                 // Create a <li> node
-        var textnode = document.createTextNode(val);         // Create a text node
-        node.appendChild(textnode);                              // Append the text to <li>
-        document.getElementById("list").appendChild(node);     // Append <li> to <ul> with id="myList"
-        document.getElementById("input").value = "";
+      var review = new Review({
+      username: req.body.username,
+      title: req.body.title,
+      type: req.body.type,
+      seasons: parseInt(req.body.seasons),
+      episodes: parseInt(req.body.episodes),
+      rank: parseInt(req.body.rank),
+      clip: title
+    }); 
 
-        node.addEventListener("mouseover", function() {
-            fetch('https://api.census.gov/data/2013/language?get=EST,LANLABEL,NAME&for=state:'+abvMap[val]+'&LAN=625').then((resp) => resp.json())
-            .then(function(data) {
-                alert("Number of spanish speakers in "+ val +" is "+numberWithCommas(data[1][0]));
-            });
-        }, false);
+    review.save(function(err) {
+      if (err) throw err;
+      io.emit('new anime listing', review);
+      return res.redirect("/"); 
+    });
+  } else {
+    res.send("not actual URL was put in");
+  }
+  });
+});
 
-        removeElement(val);
-    }
+app.get('/addComment', function(req, res) {
+  res.render('comment');
+});
+
+app.post('/api/addComment', function(req, res) {
+  Review.findOne({ username: req.body.username }, function(err, anime) {
     
-    if (states.length == 0 && document.getElementById("time").innerHTML != "EXPIRED") {
-        document.getElementById("input").style.display = 'none';
-        document.getElementById("button").style.display = 'none';
-        document.getElementById("list").style.display = 'none';
-        document.getElementById("time").style.display = 'none';
+    anime.comment.push(req.body.comm);
+    
+    anime.save(function(err) {
+      return res.redirect("/"); 
+    });
+
+});
+});
+
+app.get('/post/:name', function(req, res) {
+
+  Review.find({}, function(err, animes) {
+    var _name = req.params.name;
+    var dataOnUser = dataUtil.getDataOnUser(animes, _name);
+    var usern = dataUtil.getNames(animes);
+
+    if (!dataOnUser) return res.render('404');
+    res.render('post', {data: dataOnUser, name: _name, user : usern});
+  }); 
+  
+});
 
 
-        document.getElementById("listOfLoss").innerHTML = "You Win!";
+app.get('/nav/:word', function(req, res) {
+  Review.find({}, function(err, animes) {
+    var name = req.params.word;
+    var data = dataUtil.getSpecificData(animes, name);
+    var usern = dataUtil.getNames(animes);
 
-        clearInterval(x);
-    }
+    res.render('nav', {data : data, user : usern});
+  }); 
+});
 
-    if (document.getElementById("time").innerHTML == "EXPIRED") {
-
-        document.getElementById("input").style.display = 'none';
-        document.getElementById("button").style.display = 'none';
-        document.getElementById("list").style.display = 'none';
-
-        document.getElementById("listOfLoss").innerHTML = "States Left";
-        
-        var str =  (total-states.length);
-        document.getElementById("score").innerHTML = "Score: "+str + "/"+ total;
-
-
-        addStates();
-
-        clearInterval(x);
-    }
-        
-    }, 2);
-}
+app.get('/api/getAnime', function(req, res) {
+  Review.find({}, function(err, animes) {
+    res.send(_DATA);
+  }); 
+});
 
 
-function addStates() {
-    for (i = 0; i<states.length; i++) {
-        var state = states[i];
+app.delete('/anime/:id', function(req, res) {
+  Review.findByIdAndRemove(req.params.id, function(err, movie) {
+      if (err) throw err;
 
-        var node = document.createElement("li");                 // Create a <li> node
-        var textnode = document.createTextNode(state);         // Create a text node
-        node.appendChild(textnode);                              // Append the text to <li>
+      if(!movie){return res.send('No anime with that id');}
+      res.send('Anime deleted!');
+  });
+});
 
-        node.id = state
+app.delete('/lastComment/:id', function(req, res) {
+  Review.findOne({ _id: req.params.id }, function(err, anime) {
+    if (err) throw err;
+    if (!anime) return res.send('No movie found with that ID.');
 
-        node.addEventListener("mouseover", function() {
-            fetch('https://api.census.gov/data/2013/language?get=EST,LANLABEL,NAME&for=state:'+abvMap[state]+'&LAN=625').then((resp) => resp.json())
-            .then(function(data) {
-                alert("Number of spanish speakers in "+ state +" is "+numberWithCommas(data[1][0]));
-            });
-        }, false);
+    anime.reviews.pop();
 
-        document.getElementById("listOfLoss").appendChild(node);     // Append <li> to <ul> with id="myList"
-   
-    }
-}
+    movie.save(function(err) {
+        if (err) throw err;
+        res.send('Sucessfully added review.');
+    });
+});
+});
 
-function removeElement(ele) {
-    for (i = 0; i<states.length; i++) {
-        if (ele == states[i])
-            states.splice(i, 1);
-    }
-}
+app.get('/about', function(req,res) {
+  Review.find({}, function(err, animes) {
+    var usern = dataUtil.getNames(animes);
+
+    res.render('about', {user: usern});
+  }); 
+});
+
+http.listen(PORT, function() {
+    console.log('Listening on port 3000!');
+});
